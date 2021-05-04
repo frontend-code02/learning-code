6259081a7f432456f14ed51d6f0d7f863a8db25e
frontend-code02 <frontend.code02@gmail.com>
2021-05-04 17:19:25 -0300

    created the style of the first part of the login screen

---
+++ src/components/SocialNetworkButton/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, TouchableHighlight, Image} from 'react-native';
+
+// import { Container } from './styles';
+
+const SocialNetworkButton = ({onPress, img}) => {
+  return (
+    <View>
+      <TouchableHighlight
+        onPress={() => {}}
+        underlayColor="#6599EA"></TouchableHighlight>
+    </View>
+  );
+};
+
+export default SocialNetworkButton;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -4,19 +4,31 @@ import {View, Image, TouchableHighlight, Text, StyleSheet} from 'react-native';
 const Login = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <Image source={require('../../assets/img/login-image.png')} />
+      <View style={styles.firstItem}>
+        <Image
+          style={styles.loginImage}
+          source={require('../../assets/img/login-image.png')}
+        />
       </View>
-      <Text>Login</Text>
-      <Text>Login com redes sociais</Text>
-      <View>
-        <TouchableHighlight onPress={() => {}} underlayColor="#6599EA">
+      <Text style={styles.title}>Login</Text>
+      <Text style={styles.subtitle}>Login com redes sociais</Text>
+      <View style={styles.sectionSocialNetworks}>
+        <TouchableHighlight
+          style={styles.socialNetworkBtn}
+          onPress={() => {}}
+          underlayColor="#6599EA">
           <Image source={require('../../assets/icons/facebook-icon.png')} />
         </TouchableHighlight>
-        <TouchableHighlight onPress={() => {}} underlayColor="#6599EA">
+        <TouchableHighlight
+          style={styles.socialNetworkBtn}
+          onPress={() => {}}
+          underlayColor="#6599EA">
           <Image source={require('../../assets/icons/insta-icon.png')} />
         </TouchableHighlight>
-        <TouchableHighlight onPress={() => {}} underlayColor="#6599EA">
+        <TouchableHighlight
+          style={styles.socialNetworkBtn}
+          onPress={() => {}}
+          underlayColor="#6599EA">
           <Image source={require('../../assets/icons/google-icon.png')} />
         </TouchableHighlight>
       </View>
@@ -28,6 +40,33 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     paddingTop: 52,
+  },
+  firstItem: {
+    alignItems: 'center',
+  },
+  loginImage: {
+    width: 343,
+    height: 253,
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '500',
+    fontFamily: 'Rubik',
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 14,
+    marginBottom: 8,
+  },
+  sectionSocialNetworks: {
+    flexDirection: 'row',
+  },
+  socialNetworkBtn: {
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+    marginHorizontal: 6,
   },
 });
 
